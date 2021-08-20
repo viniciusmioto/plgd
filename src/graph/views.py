04790f161fe1f63a29a28143bb1f241421242582
Vinícius Mioto <vinemioto@gmail.com>
2021-08-19 22:27:19 -0300

    upload graph

--- src/graph/views.py
+++ src/graph/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 def index(request):
     context = {'a':1}
+    return render(request, 'index.html', context)
+
+def predict_graph(request):
+    file_object = request.FILES['file_path']
+    file_system = FileSystemStorage()
+    file_path_name = file_system.save(file_object.name, file_object)
+    file_path_name = file_system.url(file_path_name)
+    context = {'file_path_name':file_path_name}
+
     return render(request, 'index.html', context)
--- src/plgd/urls.py
+++ src/plgd/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from graph import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('^$', views.index, name='homepage'),
+    url('predict_graph', views.predict_graph, name='predict_graph'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
